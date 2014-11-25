2cf1cc8b879e95b8709964c2123b55c1a8b18590
mnhkahn <lichao0407@gmail.com>
2014-11-25 22:34:48 +0800

    add dao

---
+++ dao/dao.go
@@ -0,0 +1,17 @@
+package dao
+
+// import (
+// 	. "cyeam_post/models"
+// )
+
+// type SaverContainer interface {
+// 	AddPost(*Post)
+// 	AddPosts([]Post)
+// 	DelPost(id int)
+// 	DelPosts(source string)
+// 	UpdatePost(*Post)
+// 	GetPostById(id int) *Post
+// 	Search(q string) []Post
+// }
+
+// var
